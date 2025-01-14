bc8b6097aa4c10dd296d85513c33433222f48fb5
DvidMakesThings <s.dvid@hotmail.com>
2025-01-14 23:57:06 +0100

    update sys_setup

--- sys_setup.sh
+++ sys_setup.sh
@@ -106,4 +106,32 @@ sudo raspi-config nonint do_serial 1
 echo "Enabling 1-Wire..."
 sudo raspi-config nonint do_onewire 0
 
+# Modify /boot/firmware/config.txt to allow max current
+echo "Modifying /boot/firmware/config.txt to allow max current..."
+sudo bash -c "echo 'usb_max_current_enable=1' >> /boot/firmware/config.txt"
+
+# Modify EEPROM to allow max current
+echo "Modifying EEPROM to allow max current..."
+sudo -E rpi-eeprom-config --edit <<EOF
+PSU_MAX_CURRENT=5000
+EOF
+
 echo "Setup complete. The OLED display script and required interfaces are now enabled!"
+
+# Countdown timer in the background
+(
+    for i in {20..1}
+    do
+        echo -ne "Rebooting in $i seconds...\r"
+        sleep 1
+    done
+    echo -ne "\n"
+    sudo reboot
+) &
+
+# Prompt to press a button to reboot
+read -p "Press any key to reboot immediately or wait for the countdown to finish..." -n1 -s
+
+# Reboot to apply changes
+echo "Rebooting to apply changes..."
+sudo reboot
