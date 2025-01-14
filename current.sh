c387945668d90b00c0d31b07db781cd83c42d362
DvidMakesThings <s.dvid@hotmail.com>
2025-01-15 00:10:20 +0100

    fix

--- current.sh
+++ current.sh
@@ -1,14 +1,16 @@
+#!/bin/bash
+ 
 # Modify /boot/firmware/config.txt to allow max current
 echo "Modifying /boot/firmware/config.txt to allow max current..."
 sudo bash -c "echo 'usb_max_current_enable=1' >> /boot/firmware/config.txt"
 
 # Modify EEPROM to allow max current
 echo "Modifying EEPROM to allow max current..."
-sudo -E rpi-eeprom-config --edit <<EOF
-PSU_MAX_CURRENT=5000
-EOF
+sudo rpi-eeprom-config --out /tmp/boot.conf --config /proc/device-tree/hat/custom_0/boot.conf
+echo "PSU_MAX_CURRENT=5000" | sudo tee -a /tmp/boot.conf
+sudo rpi-eeprom-config --apply /tmp/boot.conf
 
-echo "Setup complete"
+echo "Setup complete. The OLED display script and required interfaces are now enabled!"
 
 # Countdown timer in the background
 (
